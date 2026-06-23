ef2e6ed0345a296d8c335e1f9f78ccb5642ec85b
肖全胜 <1160887335@qq.com>
2026-06-24 00:01:49 +0800

    feat(theme): replace default hero name with TextType effect
    - Inject TextType via #home-hero-info-before slot
    - Hide original .name element via CSS
    - Style .hero-split-name to match VitePress hero typography

--- docs/.vitepress/theme/components/Layout.vue
+++ docs/.vitepress/theme/components/Layout.vue
@@ -3,6 +3,7 @@ import DefaultTheme from 'vitepress/theme'
 import BackToTop from './BackToTop.vue'
 import HeroBeams from './HeroBeams.vue'
 import HeroGrainient from './HeroGrainient.vue'
+import TextType from './TextType.vue'
 </script>
 
 <template>
@@ -10,6 +11,20 @@ import HeroGrainient from './HeroGrainient.vue'
     <template #home-hero-before>
       <HeroBeams />
       <HeroGrainient />
+    </template>
+    <template #home-hero-info-before>
+      <TextType
+        :texts="[`Felix's Digital Garden`]"
+        class-name="hero-split-name"
+        :typing-speed="75"
+        :deleting-speed="50"
+        :pause-duration="1500"
+        :loop="false"
+        :show-cursor="true"
+        cursor-character="▎"
+        :cursor-blink-duration="0.5"
+        tag="h1"
+      />
     </template>
   </DefaultTheme.Layout>
   <BackToTop />
